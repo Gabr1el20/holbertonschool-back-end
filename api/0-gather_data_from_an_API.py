7fb7f687f1de7c8debbf1a99186dba69cb159797
Gabr1el20 <6327@holbertonstudents.com>
2023-08-21 10:33:06 -0700

    Added exceptions control

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,9 +11,11 @@ and displays them along with the employee's name.
 import json
 import requests
 from sys import argv
-if len(argv) != 2:
-    exit()
-user_id = argv[1]
+user_id = None
+try:
+    user_id = argv[1]
+except Exception as e:
+    pass
 todos_requests = requests.get(f"https://jsonplaceholder"
                               f".typicode.com/users/{user_id}/todos")
 user_request = requests.get(f"https://jsonplaceholder"
