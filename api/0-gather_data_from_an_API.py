7f0dd3148e7d8c11fae9e3fa5f0a6b604f2785fc
Gabr1el20 <6327@holbertonstudents.com>
2023-08-21 08:12:32 -0700

    imported exceptions

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""A script that gathers data from an API"""
+"""
+Employee TODO List Progress Script
+
+This script fetches and displays information about
+an employee's TODO list progress using a REST API.
+It retrieves the user's TODO list based on the provided employee ID,
+counts completed tasks,
+and displays them along with the employee's name.
+"""
 import json
 import requests
 from sys import argv
 user_id = argv[1]
-todos_requests = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
-user_request = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+todos_requests = requests.get(f"https://jsonplaceholder"
+                              f".typicode.com/users/{user_id}/todos")
+user_request = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/users/{user_id}")
 dicted_user = json.loads(user_request.text)
 dicted_todos = json.loads(todos_requests.text)
 completed_tasks = []
 for x in dicted_todos:
-    if x.get('completed') == True:
+    if x.get('completed') is True:
         completed_tasks.append(x.get('completed'))
-print(f"Employee {dicted_user.get('name')} is done with tasks ({len(completed_tasks)}/{len(dicted_todos)})")
-for task in dicted_todos:
-    if task.get('completed') is True:
-        print('\t ', end="")
-        print(task.get('title'))
+print(f"Employee {dicted_user.get('name')}"
+      f" is done with tasks ({len(completed_tasks)}/{len(dicted_todos)}):")
+if __name__ == "__main__":
+    """only execute the code when is main"""
+    for task in dicted_todos:
+        if task.get('completed') is True:
+            print('\t ', end="")
+            print(task.get('title'))
