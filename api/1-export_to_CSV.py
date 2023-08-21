bfaaf297d4f360455adcf244aaab4ace6633bdc7
Gabr1el20 <6327@holbertonstudents.com>
2023-08-21 11:05:37 -0700

    Task 2

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Employee TODO List Progress Script
+
+This script fetches and displays information about
+an employee's TODO list progress using a REST API.
+It retrieves the user's TODO list based on the provided employee ID,
+counts completed tasks,
+and displays them along with the employee's name.
+"""
+import csv
+import json
+import requests
+from sys import argv
+user_id = None
+try:
+    user_id = argv[1]
+except Exception as e:
+    pass
+todos_requests = requests.get(f"https://jsonplaceholder"
+                              f".typicode.com/users/{user_id}/todos")
+user_request = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/users/{user_id}")
+dicted_user = json.loads(user_request.text)
+dicted_todos = json.loads(todos_requests.text)
+completed_tasks = []
+for x in dicted_todos:
+    completed_tasks.append(x)
+if __name__ == "__main__":
+    """only execute the code when is main"""
+    with open(f"{user_id}.csv", mode="w", newline='') as f:
+        writted = csv.writer(f)
+        for x in dicted_todos:
+            writted.writerow([f'{user_id}',
+                              f'{dicted_user.get("name")}',
+                              f'{x.get("completed")}',
+                              f'{x.get("title")}'])
