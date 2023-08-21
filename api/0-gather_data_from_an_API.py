3f20952da7e3ca77e91fcf0e8b0a66a5cce4bc68
Gabr1el20 <6327@holbertonstudents.com>
2023-08-21 10:30:51 -0700

    Let´s see

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,11 +11,9 @@ and displays them along with the employee's name.
 import json
 import requests
 from sys import argv
-user_id = None
-try:
-    user_id = argv[2]
-except Exception as e:
-    pass
+if len(argv) != 2:
+    exit()
+user_id = argv[1]
 todos_requests = requests.get(f"https://jsonplaceholder"
                               f".typicode.com/users/{user_id}/todos")
 user_request = requests.get(f"https://jsonplaceholder"
@@ -27,7 +25,7 @@ for x in dicted_todos:
     if x.get('completed') is True:
         completed_tasks.append(x.get('completed'))
 print(f"Employee {dicted_user.get('name')}"
-      f" is done with tasks ({len(completed_tasks)}/{len(dicted_todos)}):")
+      f" is done with tasks({len(completed_tasks)}/{len(dicted_todos)}):")
 if __name__ == "__main__":
     """only execute the code when is main"""
     for task in dicted_todos:
